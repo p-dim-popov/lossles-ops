e9eb6adca2b13ad6c3dc6c619e394260a040368d
Petar Popov <p.dim.popov@gmail.com>
2021-03-29 21:53:00 +0300

    Closure objects are slower

--- lossless.js
+++ lossless.js
@@ -1,49 +1,45 @@
 const LOSSLESS_OPERATIONS_MAP = 1;
 const LOSSLESS_OPERATIONS_FILTER = 2;
 
-class Lossless {
-    #array;
-    #length = 0;
-    #ops = [];
-    #fns = [];
-
-    constructor(array) {
-        this.#array = array
-    }
-
-    map(fn) {
-        this.#ops[this.#length] = (LOSSLESS_OPERATIONS_MAP)
-        this.#fns[this.#length] = (fn)
-        this.#length++
-        return this
-    }
-
-    filter(fn) {
-        this.#ops[this.#length] = (LOSSLESS_OPERATIONS_FILTER)
-        this.#fns[this.#length] = (fn)
-        this.#length++
-        return this
-    }
-
-    get() {
-        const result = [];
-        let idx = 0;
-        elementIteration: for (let element of this.#array){
-            for (let i = 0; i < this.#length; i++) {
-                switch (this.#ops[i]) {
-                    case LOSSLESS_OPERATIONS_MAP:
-                        element = this.#fns[i](element);
-                        break;
-                    case LOSSLESS_OPERATIONS_FILTER:
-                        if (!this.#fns[i](element)) {
-                            continue elementIteration;
-                        }
-                        break;
+function lossless(arr) {
+    const array = arr;
+    let length = 0;
+    const ops = [];
+    const fns = [];
+    
+    return {
+        map(fn) {
+            ops[length] = (LOSSLESS_OPERATIONS_MAP)
+            fns[length] = (fn)
+            length++
+            return this
+        },
+        filter(fn) {
+            ops[length] = (LOSSLESS_OPERATIONS_FILTER)
+            fns[length] = (fn)
+            length++
+            return this
+        },
+        get() {
+            const result = [];
+            let idx = 0;
+            elementIteration: for (let element of array){
+                for (let i = 0; i < length; i++) {
+                    switch (ops[i]) {
+                        case LOSSLESS_OPERATIONS_MAP:
+                            element = fns[i](element);
+                            break;
+                        case LOSSLESS_OPERATIONS_FILTER:
+                            if (!fns[i](element)) {
+                                continue elementIteration;
+                            }
+                            break;
+                    }
                 }
+                result[idx++] = (element);
             }
-            result[idx++] = (element);
+            return result;
         }
-        return result;
     }
 }
 
@@ -52,7 +48,7 @@ const map1 = e => Math.sin(e);
 const filter2 = e => e > 0;
 
 function losslessTest(arr) {
-    return new Lossless(arr)
+    return lossless(arr)
         .filter(filter1)
         .map(map1)
         .filter(filter2)
