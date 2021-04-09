b8d8fc0191b779660ad3da659efab5ea47726a2f
Aleksandar Bonin <aleks@bonin.tech>
2021-04-09 19:18:55 +0300

    Added simple reduce.

--- interpret_results.py
+++ interpret_results.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use('dark_background')
 
 results = pd.read_csv("result.csv", header=0, names=["name", "duration"])
 
@@ -7,5 +8,12 @@ results_table = pd.concat(map(lambda x: x[1].reset_index(drop=True).pivot(column
 print(results_table.tail(-6000).agg(["mean", "std", "min", "max", "sum"]).transpose())
 print(results_table.agg(["mean", "std", "min", "max", "sum"]).transpose())
 results_table.head(40000).plot(figsize=(16,9))
-plt.savefig("result.svg", bbox_inches="tight", pad_inches=0, dpi=300)
+
+ax = plt.gca()
+fig = plt.gcf()
+
+fig.patch.set_facecolor("#0d1117")
+ax.set_facecolor("#0d1117")
+
+plt.savefig("result.svg", facecolor="#0d1117", bbox_inches="tight", pad_inches=0, dpi=300)
 plt.show()
--- lossless.js
+++ lossless.js
@@ -1,5 +1,6 @@
 const LOSSLESS_OPERATIONS_MAP = 1;
 const LOSSLESS_OPERATIONS_FILTER = 2;
+const LOSSLESS_OPERATIONS_REDUCE = 3;
 
 const filter1 = e => e % 100 === 0;
 const map1 = e => Math.sin(e);
@@ -28,7 +29,14 @@ class Lossless {
         return this
     }
 
-    get(array) {
+    reduce(fn) {
+        this.#ops[this.#length] = (LOSSLESS_OPERATIONS_REDUCE)
+        this.#fns[this.#length] = (fn)
+        this.#length++
+        return this
+    }
+
+    run(array) {
         const result = [];
         const length = array.length
         let idx = 0;
@@ -52,27 +60,43 @@ class Lossless {
         return result;
     }
 
-    getFunction() {
-        let source = "let result = []\n"
-        this.#fns.forEach(fn => {
-            source += `const ${fn.name} = ${fn.toString()}\n`  
-        })
-        source += "const len = array.length\n"
-        source += "for (let i = 0, idx = 0; i < len; i++){\n"
-        source += "\tlet element = array[i]\n"
+    compile() {
+        let source = `
+let result = []
+${this.#fns.reduce((acc, fn, i) => acc + `const fn${i} = ${fn.toString()};\n`, '')}
+const len = array.length;
+let acc = 0;
+for (let i = 0, idx = 0; i < len; i++) {
+    let element = array[i];
+`;
         for (let op = 0; op < this.#length; op++) {
             switch (this.#ops[op]) {
                 case LOSSLESS_OPERATIONS_MAP:
-                    source += `\telement = ${this.#fns[op].name}(element)\n`;
+                    source += `\telement = fn${op}(element)\n`;
                     break;
                 case LOSSLESS_OPERATIONS_FILTER:
-                    source += `\tif(!${this.#fns[op].name}(element)) { continue }\n`;
+                    source += `\tif(!fn${op}(element)) { continue }\n`;
                     break;
+                case LOSSLESS_OPERATIONS_REDUCE:
+                    if(op !== this.#length - 1) {
+                        throw "TODO: Too bad";
+                    }
+                    source += `\tacc = fn${op}(acc, element)`
             }
         }
-        source += "\tresult[idx++] = element;\n"
-        source += "}\n"
-        source += "return result\n"
+        if(this.#ops[this.#length - 1] === LOSSLESS_OPERATIONS_REDUCE) {
+            source += `
+}
+return acc;
+`;
+        }else{
+            source += `
+    result[idx++] = element;
+}
+return result;
+`;
+        }
+
         console.error(source)
 
         return new Function("array", source);
@@ -80,20 +104,28 @@ class Lossless {
 }
 
 
-
 function losslessTest(arr) {
     return new Lossless()
         .filter(filter1)
         .map(map1)
         .filter(filter2)
-        .get(arr)
+        .run(arr)
 }
+
+new Lossless()
+    .filter((a) => a > 5)
+    .map(map1)
+    .reduce((a,b) => a + b, 0)
+    .compile()
+
 
 let losslessCodeGenFunction = new Lossless()
     .filter(filter1)
     .map(map1)
     .filter(filter2)
-    .getFunction()
+    .compile()
+
+console.error("" + losslessCodeGenFunction)
 
 function losslessCodeGenTest(arr) {
     return losslessCodeGenFunction(arr);
